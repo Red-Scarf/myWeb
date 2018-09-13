b40f09bff3536e61fbe3eb07efb223805373a29a
red_scard <705762646@qq.com>
2018-09-13 22:43:32 +0800

    性能优化例子

--- app/Admin/Controllers/PostController.php
+++ app/Admin/Controllers/PostController.php
@@ -10,7 +10,9 @@ class PostController extends Controller
     public function index()
     {
         // 不使用scope且status为0，按时间倒序排列
-        $posts = Post::withoutGlobalScope('available')->where('status', 0)->orderBy('created_at', 'desc')->paginate(10);
+        $posts = Post::withoutGlobalScope('available')->where('status', 0)->orderBy('created_at', 'desc')->with('user')->paginate(10);
+
+        // $posts->load('user'); // 与上一行中的with功能相同
 
         return view('admin.post.index', compact('posts'));
     }
